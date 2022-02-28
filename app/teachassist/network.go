cf6944e92320354d2a729e0c18c5a3ce875a51b0
George Balaci <alexb2404@hotmail.com>
2022-02-27 21:35:24 -0500

    add ability to get all courses and courses by id

--- app/teachassist/network.go
+++ app/teachassist/network.go
@@ -3,6 +3,7 @@ package teachassist
 import (
 	"TeachAssistApi/app"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"time"
@@ -20,10 +21,10 @@ func loginUser(username, password string) (UserMetadata, error) {
 		},
 	}
 	req, err := http.NewRequest(method, url, payload)
-
 	if err != nil {
 		return UserMetadata{}, app.CreateError(app.NetworkError)
 	}
+
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	res, err := client.Do(req)
@@ -55,4 +56,48 @@ func loginUser(username, password string) (UserMetadata, error) {
 		SessionToken:  sessionToken,
 		SessionExpiry: sessionExpiry,
 	}, nil
+}
+
+func getAllCourses(user *UserMetadata) (*string, error) {
+	url := fmt.Sprintf("https://ta.yrdsb.ca/live/students/listReports.php?student_id=%s", user.StudentId)
+	return authenticatedRequest(url, user)
+}
+
+func getCourseById(id string, user *UserMetadata) (*string, error) {
+	url := fmt.Sprintf("https://ta.yrdsb.ca/live/students/viewReport.php?subject_id=%s&student_id=%s", id, user.StudentId)
+	return authenticatedRequest(url, user)
+}
+
+func authenticatedRequest(url string, user *UserMetadata) (*string, error) {
+	method := "GET"
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, app.CreateError(app.NetworkError)
+	}
+
+	req.Header.Add("Cookie", fmt.Sprintf("session_token=%s; student_id=%s", user.SessionToken, user.StudentId))
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, app.CreateError(app.NetworkError)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, app.CreateError(app.NetworkError)
+	}
+
+	webData := string(body)
+	if webData == "" || strings.Contains(webData, "Login") {
+		return nil, app.CreateError(app.AuthError)
+	}
+
+	return &webData, nil
 }
